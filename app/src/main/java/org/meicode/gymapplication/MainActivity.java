eb96eea8a59f6193e9c6eba99946cda970f8a334
Meisam <Meisam@meiCode.org>
2020-06-02 13:23:56 +0400

    The source code for the video called 'Challenge - Gym Application (Part 3)' in the Android App Development Master Class for 2020

---
+++ app/src/main/java/org/meicode/gymapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package org.meicode.gymapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private Button btnPlans, btnAllTrainings, btnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        Utils.initTrainings();
+        // TODO: 4/19/2020 Create onClickListeners
+
+        btnAllTrainings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllTrainingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPlans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlanActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        Log.d(TAG, "initViews: started");
+        btnPlans = findViewById(R.id.btnPlanActivity);
+        btnAllTrainings = findViewById(R.id.btnAllActivities);
+        btnAbout = findViewById(R.id.btnAbout);
+    }
+}
